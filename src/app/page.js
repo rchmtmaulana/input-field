9ecef4793245f10230de6cb46f1ff73ce838da12
rchmtmaulana <rachmatmaulana81669@gmail.com>
2023-11-15 00:12:57 +0800

    feat: set input field

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,62 @@
+'use client'
+import { useState } from 'react'
+
 import Image from 'next/image'
-import styles from './page.module.css'
+import './globals.css'
 
 export default function Home() {
+
+  
+  const [nama, setNama] = useState('Nama Anda Disini')
+  const [inputNamaBaru, setInputNamaBaru] = useState('')
+
+  function handlerGantiNama() {
+    setNama(inputNamaBaru)
+  }
+
+  function inputHandler(value) {
+    setInputNamaBaru(value)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className='body'>
+      <div className="banner-container">
+        {/* kartunya */}
+        <div className="header-banner-wrapper">
+          {/* foto profil dan nama */}
+          <div className="profile-header-banner">
+            {/* foto profil */}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/assets/Logo-Unhas.png"
+              alt="Universitas Hasanuddin"
+              fill
+              objectFit='contain'
             />
-          </a>
+          </div>
+          <div className="content-header-banner">
+            {/* nama dan kawan2 */}
+            <h1>{nama}</h1>
+            <div className="bio-nim-header-banner">
+            {/* NIM dan BIO */}
+            <p style={{marginBottom: '8px'}}>Universitas Hasanuddin</p>
+            <p>Teknik Informatika</p>
+          </div>
+          </div>
+        </div>
+        <input className='input-field'
+          placeholder='Masukkan Nama Anda'
+          onInput={(value) => inputHandler(value.target.value)}
+          ></input>
+        <div className="cta-banner-wrapper">
+          {/* tombol cta */}
+            <button className='cta-button'
+              onClick={() => {
+                handlerGantiNama();
+              }}>
+              Ganti
+            </button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
